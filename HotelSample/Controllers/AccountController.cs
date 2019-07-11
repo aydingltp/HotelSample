ce3193763e3468ba744545854e8bcab783701241
aydingltp <aydngltp@gmail.com>
2019-07-11 15:46:59 +0300

    Login and SignIn

---
+++ HotelSample/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelSample.Models;
+using HotelSample.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelSample.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel user)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignIn(User user)
+        {
+            return View();
+        }
+    }
+}
